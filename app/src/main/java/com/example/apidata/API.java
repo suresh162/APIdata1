a996651ecae6030a8f9d8d0b4d4902a09de8d629
Softwarica College Lab <softwarica@softwarica.com>
2019-05-12 11:33:13 +0545

    and additonal search bar was added to search through th inserted data

--- app/src/main/java/com/example/apidata/API.java
+++ app/src/main/java/com/example/apidata/API.java
@@ -5,9 +5,14 @@ import java.util.List;
 import model.Employee;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface API{
     @GET("employees")
     Call<List<Employee>> getEmployee();
 
+    @GET("employee/{empID}")
+    Call<Employee>getEmployeeByID(@Path("empID")int empID);
+
 }
+
